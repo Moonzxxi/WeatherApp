1ede4092ebf15807d16ea04683b551eb458015d9
Moonzxxi <56330795+Moonzxxi@users.noreply.github.com>
2021-04-11 19:29:14 -0400

    Add files via upload

---
+++ WeatherApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WeatherApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CurrentWeatherPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
